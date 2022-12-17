7b5d4219ed5e458fb73de9d63e44c645dbdcfe26
JuliyaElatova <elatova_jul@mail.ru>
2022-12-17 12:06:51 +0500

    учимся работать в пайтоне

---
+++ HelloPython/lec.py
@@ -0,0 +1,90 @@
+# print("hello world")
+#value = None
+# None - для объявления переменной, тип которой мы не знаем
+#print (type(value))
+#a = 123
+#b = 1.23
+#print (type(a)) 
+#type - чтобы узнать тип переменной
+#print (type(b))
+#value = 1245
+#s = 'hello'
+#s = 'hello \'world'
+#s = 'hello \n world'
+# print (s) # вывод строки 
+#print (a,'-',b,'-', s)
+#print('{1} - {2} - {0}'.format(a, b, s))
+#print(f'{a} - {b} - {s}')
+#f = True
+#print(f)
+
+#Ввод и вывод данных
+#print, input
+#print('Введите a')
+#a = int (input())
+#print('Введите b')
+#b = int (input())
+#print(a, b)
+#print('{} {}'.format(a, b))
+#print(f'{a} {b}')
+
+#сумма двух чисел
+#print(a, ' + ',  b, ' = ', a+b)
+
+#print('Введите a')
+#a = float (input())
+#print('Введите b')
+#b = float (input())
+#print(a, ' + ',  b, ' = ', a+b)
+
+#Логические операции
+#
+#a = 1 < 4 and 5 > 2
+#a = 1 == 2
+#a = 1 != 2
+#print(a)
+
+#a = 'qwe'
+#b = 'qwet'
+#a = [1,2]
+#b = [1,3]
+#print(a==b)
+
+#управляющие констукции if else
+#a = int(input('a = '))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
+
+# Грокаем алгоритмы Адитья Бхаргава
+# Задача на  уроке: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+# *Примеры:* 
+    
+   # - 5, 25 -> да
+   # - 4, 16 -> да
+   # - 25, 5 -> да
+   # - 8,9 -> нет
+#print('Введите число a')
+#a = int(input ())
+#print('Введите число b')
+#b = int(input ())
+
+#if a == b**2 or b == a**2:
+#    print('да')
+#else:
+#   print('нет')
+
+#2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.   
+#Примеры:
+#    - 1, 4, 8, 7, 5 -> 8
+#   - 78, 55, 36, 90, 2 -> 90
+
+list = [1, 4, 8, 7, 5]
+print(list)
+ind = 0
+if f[0] > f[1]:
+    print()
+else:
+    print()
