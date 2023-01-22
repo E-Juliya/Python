f643b132328ea4d1b81caa8dc8b7052b4a3e4ee2
JuliyaElatova <elatova_jul@mail.ru>
2023-01-22 11:42:07 +0500

    Python: от простого к сложному

---
+++ HelloPython/calc/model.py
@@ -0,0 +1,16 @@
+# Python: от простого к сложному 
+# Задача 0
+x = 0
+y = 0
+
+def  init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+#init(11, 22)
+#print(x)
+#print(y)
+
+def sum():
+    return x + y
